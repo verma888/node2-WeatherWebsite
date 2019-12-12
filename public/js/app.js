323f3db1bdc21f044b36af00f3918d786e9aa0a3
Gourav Verma <51471939+verma888@users.noreply.github.com>
2019-12-12 12:23:50 +0530

    About page updated

--- public/js/app.js
+++ public/js/app.js
@@ -3,10 +3,15 @@ const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
 
-messageOne.textContent = "Loading.."
+
 weatherForm.addEventListener('submit',(e)=>{
-    const location = search.value;
     e.preventDefault();
+
+    const location = search.value;
+
+    messageOne.textContent = "Loading.."
+    messageTwo.textContent = ''
+    
     fetch('/weather?address= ' + location).then((response)=>{
         response.json().then((data)=>{
             if(data.error){
